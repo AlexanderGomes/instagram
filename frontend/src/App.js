b050ec714f69cd7fe30f17829791e1b43ffaa5ee
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2022-09-26 19:35:50 -0700

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import {Profile, Home, Register, Login} from './pages'
+import {BrowserRouter as Router, Routes} from "react-router-dom"
 
 function App() {
   return (
